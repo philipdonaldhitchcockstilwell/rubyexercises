1e9ff1801c88ec8f252619d56b4b20cd7ec1ecfb
Philip <philip.stilwell32@gmail.com>
2020-12-03 16:24:23 -0800

    Putting tests up on repo

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -5,32 +5,42 @@ end
 
 def subtract(a, b)
   # return the result of subtracting a from b
+  a - b
 end
 
 def multiply(a, b)
   # return the result of multiplying a times b
+  a * b
 end
 
 def divide(a, b)
   # return the resut of dividing a by b
+  a / b
 end
 
 def remainder(a, b)
   # return the remainder of dividing a by b using the modulo operator
+  a % b
 end
 
 def float_division(a, b)
   # return the result of dividing a by b as a float, rather than an integer
+  a.to_f + b.to_f
 end
 
 def string_to_number(string)
   # return the result of converting a string into an integer
+  a.to_s
 end
 
 def even?(number)
   # return true if the number is even
+  if a % 2 == 0
+    return true
 end
 
 def odd?(number)
   # return true if the number is odd
+  if a % 2 != 0
+    return true
 end
